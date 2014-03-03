7afca7165975d909ac5d552912e8bb70c64b12bc
Soundararajan Dhakshinamoorthy <er.soundararajan@gmail.com>
2014-03-03 10:40:50 +0530

    Intial commit of the Arduino IDE Wrapper for .NET environment

---
+++ ArduinoIDEWrapper/ArduinoExample.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+
+namespace Arduino.IDE.Wrapper
+{
+    public sealed class ArduinoExample
+    {
+        string fullPath;
+        public ArduinoExample(string fullPathToExampleFile)
+        {
+            if (!File.Exists(fullPathToExampleFile))
+                throw new FileNotFoundException("The specified file does not exist");
+            fullPath = fullPathToExampleFile;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(fullPath);
+            }               
+        }
+
+        public string Description
+        {
+            get
+            {
+                return GetDescription(fullPath);
+            }
+        }
+
+        private string GetDescription(string fullPath)
+        {
+            using (StreamReader reader = new StreamReader(fullPath))
+            {
+                char[] buffer = new char[1024]; //Read 1 Kb of information from the file.
+                reader.ReadBlock(buffer, 0, 1024);
+                return new String(buffer);
+            }
+        }
+
+        public string FullPath { get { return fullPath; } }
+
+        string category;
+        public string Category
+        {
+            get
+            {
+                if (category == null)
+                {
+                    category = ComputeCategoryFromFilePath();
+                }
+                return category;
+            }
+        }
+
+        private string ComputeCategoryFromFilePath()
+        {
+            var examplesDir = "examples";
+            var path = fullPath;
+            do
+            {
+                var dir = Path.GetDirectoryName(path);
+                if (String.Equals(dir, examplesDir, StringComparison.OrdinalIgnoreCase))
+                {
+
+                }
+                path = Directory.GetParent(path).FullName;
+            }
+            while (string.IsNullOrEmpty(path));
+        }
+        public string SubCategory { get; private set; }
+    }
+}
---
+++ ArduinoIDEWrapper/ArduinoIDE.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Arduino.IDE.Wrapper
+{
+    public sealed class ArduinoIDE
+    {
+        string installBasePath;
+        public ArduinoIDE(string installBasePath)
+        {
+            if (string.IsNullOrEmpty(installBasePath))
+                throw new ArgumentException("installBasePath");
+
+            if (!Directory.Exists(installBasePath))
+                throw new DirectoryNotFoundException("The specified installation base directory doesn't exist");
+
+            this.installBasePath = installBasePath;
+        }
+
+        private Version version;
+        public Version Version
+        {
+            get
+            {
+                if (version == null)
+                {
+                    version = InitializeVersion();
+                }
+                return version;
+            }
+        }
+
+        public StreamReader ReleaseNotes
+        {
+            get
+            {
+                var path = Path.Combine(installBasePath, "revisions.txt");
+                if (File.Exists(path))
+                    return new StreamReader(path);
+                else
+                    throw new FileNotFoundException(string.Format("Unable to find the file {0} to retrieve release notes"));
+            }
+        }
+
+        public IEnumerable<ArduinoExample> EnumerateExamples()
+        {
+            var basePathToExamples = Path.Combine(installBasePath, "examples");
+            if (!Directory.Exists(basePathToExamples))
+                return Enumerable.Empty<ArduinoExample>();
+
+            var examples = RecurseEnumerateExamples(basePathToExamples);
+        }
+
+        private IEnumerable<ArduinoExample> RecurseEnumerateExamples(string examplesDirectory)
+        {
+            foreach (var file in Directory.EnumerateFiles(examplesDirectory, "*.ino", SearchOption.AllDirectories))
+            {
+                yield return new ArduinoExample(file);
+            }
+        }
+
+
+        private Version InitializeVersion()
+        {
+            var path = Path.Combine(installBasePath, "lib", "version.txt");
+            if (File.Exists(path))
+            {
+                var version = File.ReadAllText(path);
+                return new Version(version);
+            }
+            else
+                throw new FileNotFoundException(string.Format("Unable to find the file {0} to retrieve the revision information of the Arduino installation", path));
+        }
+    }
+}
